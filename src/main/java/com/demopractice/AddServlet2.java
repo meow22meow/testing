b054f0b9780c719c77851167b49e8a6fd75d1bd3
Shabnam Begum <shabnambegumahmed@gmail.com>
2023-01-17 20:20:13 +0530

    Updated file using cookies and sessions

--- src/main/java/com/demopractice/AddServlet2.java
+++ src/main/java/com/demopractice/AddServlet2.java
@@ -1,20 +1,34 @@
 package com.demopractice;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet2 extends HttpServlet{
-	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException 
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException 
 	{
 		int i=Integer.parseInt(req.getParameter("num1"));
 		int j=Integer.parseInt(req.getParameter("num2"));
 		int k=i+j;
-		PrintWriter out=res.getWriter();
-		out.println("result is: "+k);
+		/*PrintWriter out=res.getWriter();
+		out.println("result is: "+k);*/
+		//req.setAttribute("k",k);
+		
+		
+		/*HttpSession session=req.getSession();
+		session.setAttribute("k", k);*/
+		
+		/*RequestDispatcher rd=req.getRequestDispatcher("sq");
+		rd.forward(req,res);*/
+		//res.sendRedirect("sq?k="+k);
+		
+		Cookie cookie=new Cookie("k",k+"");
+		res.addCookie(cookie);
 	}
 	
 	/*public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException 
@@ -25,5 +39,6 @@ public class AddServlet2 extends HttpServlet{
 		PrintWriter out=res.getWriter();
 		out.println("result is: "+k);
 	}*/
+	
 
 }
--- src/main/java/com/demopractice/Square.java
+++ src/main/java/com/demopractice/Square.java
@@ -1,7 +1,34 @@
 package com.demopractice;
 
-import HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Cookie;
+
+
 
 public class Square extends HttpServlet {
-
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		 Cookie cookies[]=req.getCookies();
+		
+		int k=0;
+		for(Cookie c :cookies)
+		{
+			if(c.getName().equals("k"))
+				k=Integer.parseInt(c.getValue());
+		}
+		/*int k=(int)req.getAttribute("k");*/
+		//int k=Integer.parseInt(req.getParameter("k"));
+		/*HttpSession session=req.getSession();
+		int k=(int)session.getAttribute("k");*/
+		k=k*k;
+		PrintWriter out=res.getWriter();
+		out.println("Result is "+k);
+		System.out.println("Called");
+	}
 }
